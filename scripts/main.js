7df0686d2a830c098cad80cbbb0585feb30ecab4
Rustam <rustam.kireev@gmail.com>
2016-02-22 03:48:36 +0300

    создаем 3d пирамидки

--- scripts/main.js
+++ scripts/main.js
@@ -1,95 +1,19 @@
 "use strict";
 
 $(function() {
+    var value = 0;
+    var $block = $('.shape-wrapper > .triangle');
 
-    // слайдер плиточного элемента
-    // работает если все пункты одинаковой ширины
-    var tileSlider = $('.tiles.slider'),
-        tileSliderList = tileSlider.children('ul'),
-        tileSliderItem = tileSlider.find('li'),
-        tileSliderLastItemWidth = tileSliderItem.eq(tileSliderItem.length - 1).outerWidth(true),
-        tileSliderPrev = tileSlider.find('.prev'),
-        tileSliderNext = tileSlider.find('.next'),
-        tileSliderListWidth = 0,
-        tileSliderPosition = 0;
+    setInterval(function() {
+        //console.log(value);
+        value++
 
-    // считаем ширину всех пунктов
-    for(var index = 0; index < tileSliderItem.length; index++) {
-        tileSliderListWidth += tileSliderItem.eq(index).outerWidth(true);
-    }
+        $block.css({
+            //"transform": "rotateX(" + value + "deg)"
+        });
 
-    tileSliderList.width(tileSliderListWidth);
-
-    // если нажали next
-    tileSliderNext.click( function() {
-        if(!$(this).hasClass('disabled')) {
-            tileSliderPosition -= tileSliderLastItemWidth;
-            tileSliderList.css({'left': tileSliderPosition});
-            setButsStatus();
-        }
-    });
-
-    // если нажали prev
-    tileSliderPrev.click( function() {
-        if(!$(this).hasClass('disabled')) {
-            tileSliderPosition += tileSliderLastItemWidth;
-            tileSliderList.css({'left': tileSliderPosition});
-            setButsStatus();
-        }
-    });
-
-    function setButsStatus() {
-        // проверяем возможность прокрутки влево
-        if (tileSliderPosition < 0) {
-            tileSliderPrev.removeClass('disabled');
-        } else {
-            tileSliderPrev.addClass('disabled');
-        }
-
-        // проверяем возможность прокрутки вправо
-        if (-tileSlider.width() < tileSliderPosition) {
-            tileSliderNext.removeClass('disabled');
-        } else {
-            tileSliderNext.addClass('disabled');
-        }
-    }
+    }, 30)
 
 
-
-    // появление подменю при наведение на пункты меню
-    // срабатывает только если находит обертку для основного пункта
-    $('nav .main-menu > li').hover(
-        function() {
-            if ($('.sub-menu').eq($(this).index()).length) {
-                $('nav').addClass('hovered');
-
-                $(this).addClass('hover');
-                $('.sub-menu').eq($(this).index()).show();
-            }
-        },
-
-        function() {
-            $('nav').removeClass('hovered');
-
-            $(this).removeClass('hover');
-            $('.sub-menu').eq($(this).index()).hide();
-        }
-    );
-
-    $('nav .sub-menu').hover(
-        function(){
-            $('nav').addClass('hovered');
-
-            $('.main-menu li').eq($(this).index() - 1).addClass('hover');
-            $(this).show();
-        },
-
-        function(){
-            $('nav').removeClass('hovered');
-
-            $('.main-menu li').eq($(this).index() - 1).removeClass('hover');
-            $(this).hide();
-        }
-    );
 
 });;
